7a17d6731edc1e86d23e4c408f7635e272ebaed0
Neil <152786225+theneilproject@users.noreply.github.com>
2025-04-03 14:32:44 +0900

    25.04.03 - Range Slider Horizontal/Vertical hover radius 범위까지 적용 완료

--- SampleOpenTK/Pages/PageControl.cs
+++ SampleOpenTK/Pages/PageControl.cs
@@ -13,20 +13,22 @@ namespace SampleOpenTK.Pages
     {
         GoProgress prgs;
         GoSlider sldh, sldv;
-        GoRangeSlider rsld;
+        GoRangeSlider rsldh, rsldv;
         public PageControl()
         {
             Name = "PageControl";
 
             prgs = new GoProgress { Left = 10, Top = 10, Width = 200, Height = 40 };
             sldh = new GoSlider { Left = 10, Top = 60, Width = 200, Height = 40 };
-            sldv = new GoSlider { Left = 210, Top = 10, Width = 40, Height = 200, Direction = GoDirectionHV.Vertical };
-            rsld = new GoRangeSlider { Left = 10, Top = 110, Width = 200, Height = 40 };
+            sldv = new GoSlider { Left = 220, Top = 0, Width = 40, Height = 200, Direction = GoDirectionHV.Vertical };
+            rsldh = new GoRangeSlider { Left = 10, Top = 110, Width = 200, Height = 40 };
+            rsldv = new GoRangeSlider { Left = 280, Top = 0, Width = 40, Height = 200, Direction = GoDirectionHV.Vertical };
 
             Childrens.Add(prgs);
-            Childrens.Add(sldv);
             Childrens.Add(sldh);
-            Childrens.Add(rsld);
+            Childrens.Add(sldv);
+            Childrens.Add(rsldh);
+            Childrens.Add(rsldv);
 
             sldv.ValueChanged += (o, s) => prgs.Value = sldv.Value;
         }
